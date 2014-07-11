07b9b370ec97766ded10e85e2062f038bcf523b0
Tom Murphy <tom@turphdesigns.com>
2014-07-11 05:20:02 -0600

    Sync again
    So out of waco….

---
+++ includes/header.php
@@ -0,0 +1,10 @@
+<div class="ribbon">
+	<div class="centerWrap">
+		<div class="logo"><img src="img/t-logoL.png" width="27" height="42" alt="T LogoL"></div>
+		<span class="mast">turphdesigns</span>
+	</div>
+</div>
+<div class="mainNav centerWrap">
+	<a id="work" href="index.html">articles & work</a>
+	<a id="about" href="about.html">about</a>
+</div>
