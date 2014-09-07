506c2f562fc15e58829415ee5de1b4bdde6baa5e
Tom Murphy <tom@turphdesigns.com>
2014-09-07 16:43:57 -0600

    Header & Grid

--- includes/header.php
+++ includes/header.php
@@ -1,10 +1,9 @@
 <div class="ribbon">
-	<div class="centerWrap">
-		<div class="logo"><img src="img/t-logoL.png" width="27" height="42" alt="T LogoL"></div>
-		<span class="mast">turphdesigns</span>
+	<div class="center">
+		<div class="logo"><a href="index.php"><img src="img/v3-logo.png" alt="T LogoL"><span class="mast">turphdesigns</span></a></div>
+		<div class="mainNav">
+			<a id="work" href="index.php">articles & work</a>
+			<a id="about" href="about.html">about</a>
+		</div>
 	</div>
-</div>
-<div class="mainNav centerWrap">
-	<a id="work" href="index.html">articles & work</a>
-	<a id="about" href="about.html">about</a>
 </div>
---
+++ includes/homeHero.php
@@ -0,0 +1,11 @@
+<div class="homeHero">
+	<div class="title">
+		<h3>Senior Product Designer</h3>
+		<p>Product strategy to visual design</p>
+	</div>
+	<div class="heroRibbon">
+		<div class="heroFooter">
+		stuff here
+		</div>
+	</div>
+</div>
