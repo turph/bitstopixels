cd3e65db371f174706e5dbd695520548a37bdb13
Tom Murphy <tom@turphdesigns.com>
2015-04-18 11:13:37 -0600

    Removing old php

--- includes/header.php
+++
@@ -1,9 +0,0 @@
-<div class="ribbon">
-	<div class="center">
-		<div class="logo"><a href="index.php"><img src="img/v3-logo.png" alt="T LogoL"><span class="mast">turphdesigns</span></a></div>
-		<div class="mainNav">
-			<a id="work" href="index.php">articles & work</a>
-			<a id="about" href="about.html">about</a>
-		</div>
-	</div>
-</div>
